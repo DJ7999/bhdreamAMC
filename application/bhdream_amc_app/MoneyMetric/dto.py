2953dc82d459a025699c62fe1b124d8fbb090bc0
Rohit joshi <bhdream0000@gmail.com>
2023-11-05 19:23:31 +0530

    created money metric service

--- application/bhdream_amc_app/MoneyMetric/dto.py
+++ application/bhdream_amc_app/MoneyMetric/dto.py
@@ -1,16 +1,106 @@
 from user_controller.models import EquitySymbol  # Import the EquitySymbol model
 
 class EquityDTO:
-    def __init__(self, equity_symbol, amount_invested):
+    def __init__(self, equity_symbol, amount_invested,):
         self.equity_symbol = equity_symbol  # equity_symbol should be an instance of EquitySymbol
         self.amount_invested = amount_invested
+        self.cagr = None
+        self.risk = None
+        self.optimal_weight=None
 
     def get_equity_symbol(self):
         return self.equity_symbol
 
+    def get_cagr(self):
+        return self.cagr
+
+    def get_risk(self):
+        return self.risk
+    
+    def set_cagr(self, cagr):
+        self.cagr = cagr
+
+    def set_risk(self, risk):
+        self.risk = risk
+    
+    def set_optimal_weight(self, optimal_weight):
+        self.optimal_weight = optimal_weight
+
 class PortfolioDTO:
     def __init__(self, equity_data_list):
         self.equities = equity_data_list  # equities should be a list of EquityDTO instances
+        self.has_metrics = False
+        self.is_optimised=False
+        self.current_return=None
+        self.current_risk=None
+        self.optimised_return=None
+        self.optimised_risk=None
+        self.total_portfolio_value=sum(equity.amount_invested for equity in equity_data_list)
+    def get_equities(self):
+        return self.equities
     
+    def set_equities(self,equities):
+        self.equities=equities
+
+    def set_optimised_return(self,optimised_return):
+        self.optimised_return=optimised_return
+    
+    def set_optimised_risk(self,optimised_risk):
+        self.optimised_risk=optimised_risk
+
+    def set_current_return(self,current_return):
+        self.current_return=current_return
+    
+    def set_current_risk(self,current_risk):
+        self.current_risk=current_risk
+
     def get_equity_symbols(self):
         return [equity.get_equity_symbol() for equity in self.equities]
+    
+    def Enable_metrics(self):
+        self.has_metrics=True
+
+    def Is_metrics_Enable(self):
+        return self.has_metrics
+    
+    def get_cagrs(self):
+        return [equity.get_cagr() for equity in self.equities]
+    
+    def get_risks(self):
+        return [equity.get_risk() for equity in self.equities]
+
+    def get_equity(self, equity_symbol):
+        for equity in self.equities:
+            if equity.get_equity_symbol() == equity_symbol:
+                return equity
+        # If equity_symbol is not found, you can return None or raise an exception
+        return None
+    
+    def get_total_portfolio_value(self):
+        return self.total_portfolio_value
+    
+    def set_cagrs(self, cagr_updates):
+        for update in cagr_updates:
+            equity_symbol = update.key
+            new_cagr = update.value
+            for equity in self.equities:
+                if equity.get_equity_symbol() == equity_symbol:
+                    equity.set_cagr(new_cagr)
+                    break
+    
+    def set_risks(self, risk_updates):
+        for update in risk_updates:
+            equity_symbol = update.key
+            new_cagr = update.value
+            for equity in self.equities:
+                if equity.get_equity_symbol() == equity_symbol:
+                    equity.set_cagr(new_cagr)
+                    break
+    
+class KeyValuePairDTO:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return f"Key: {self.key}, Value: {self.value}"
--- application/bhdream_amc_app/MoneyMetric/money_metric_insight_service.py
+++ application/bhdream_amc_app/MoneyMetric/money_metric_insight_service.py
@@ -1,9 +1,96 @@
 from historical_data_engine import get_historical_data
+from dto import KeyValuePairDTO
+import numpy as np
+from scipy.optimize import minimize
 
-def get_metrics(portfolio):
+def populate_portfolio_metrics(portfolio):
     symbol_list=portfolio.get_equity_symbols()
     historical_data=get_historical_data(symbol_list).Close
-    #calculate_metrics(historical_data)
+    returns, risks  =calculate_metrics(historical_data)
+    portfolio_return, portfolio_risk=current_portfolio_risk_return(portfolio.get_equities(),portfolio.get_total_portfolio_value())
+    portfolio.set_cagrs(returns)
+    portfolio.set_risks(risks)
+    portfolio.Enable_metrics()
+    portfolio.set_current_return(portfolio_return)
+    portfolio.set_current_risk(portfolio_risk)
+    return portfolio
 
-#def calculate_metrics(historical_data):
+def current_portfolio_risk_return(equity_list, total_portfolio_value):
+    # Calculate the weights of each equity in the portfolio
+    weights = [equity.amount_invested / total_portfolio_value for equity in equity_list]
 
+    # Calculate the expected returns of each equity
+    expected_returns = [equity.cagr for equity in equity_list]
+
+    # Calculate the covariance matrix (assuming independent assets)
+    risk_values = [equity.risk for equity in equity_list]
+    cov_matrix = np.diag(np.square(risk_values))
+
+    # Calculate the portfolio return
+    portfolio_return = np.dot(weights, expected_returns)
+
+    # Calculate the portfolio risk (standard deviation)
+    portfolio_risk = np.sqrt(np.dot(weights, np.dot(cov_matrix, weights)))
+
+    return portfolio_return, portfolio_risk
+
+def calculate_metrics(historical_data):
+    log_ret=np.log(historical_data / historical_data.shift(1))
+    annual_mu_df=log_ret.mean()*12
+    sigma=log_ret.std()
+    annual_std_df=sigma*np.sqrt(12)
+    risk_list=[KeyValuePairDTO(index, value) for index, value in annual_std_df.items()]
+    cagr_list=[KeyValuePairDTO(index, np.exp(value) - 1) for index, value in annual_mu_df.items()]
+    return cagr_list,risk_list
+
+def get_optimised_portfolio(portfolio):
+    if not portfolio.is_metrics_enabled():
+        portfolio = populate_portfolio_metrics(portfolio)
+    optimal_equity_list,optimised_return,optimised_risk=optimize_portfolio(equity_data_list=portfolio.get_equities())
+    portfolio.set_equities(optimal_equity_list)
+    portfolio.set_optimised_return(optimised_return)
+    portfolio.set_optimised_risk(optimised_risk)
+    portfolio.is_optimised=True
+    return portfolio
+
+def optimize_portfolio(equity_data_list, risk_free_rate=0.03):
+    # Step 2: Calculate expected returns
+    expected_returns = [equity.get_cagr() for equity in equity_data_list]
+
+    # Step 3: Calculate the covariance matrix (diagonal for independent equities)
+    risk_values = [equity.get_risk() for equity in equity_data_list]
+    cov_matrix = np.diag(np.square(risk_values))
+
+    # Define the negative Sharpe ratio objective function
+    def negative_sharpe(weights, expected_returns, cov_matrix, risk_free_rate):
+        portfolio_return = np.sum(weights * expected_returns)
+        portfolio_stddev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+        sharpe_ratio = (portfolio_return - risk_free_rate) / portfolio_stddev
+        return -sharpe_ratio
+
+    # Define constraints (e.g., sum of weights equals 1)
+    constraints = ({'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1})
+
+    # Define bounds (weights between 0 and 1)
+    bounds = tuple((0, 1) for asset in range(len(equity_data_list)))
+
+    # Set initial guess for weights
+    initial_weights = [1 / len(equity_data_list)] * len(equity_data_list)
+
+    # Step 4: Solve the optimization problem
+    result = minimize(negative_sharpe, initial_weights, args=(expected_returns, cov_matrix, risk_free_rate),
+                      method='SLSQP', bounds=bounds, constraints=constraints)
+
+    # Step 5: Extract the optimal weights
+    optimal_weights = result.x
+    for i, equity in enumerate(equity_data_list):
+        equity.weight = optimal_weights[i]
+    
+    portfolio_return = np.sum(optimal_weights * expected_returns)
+
+    # Step 7: Calculate the portfolio risk (standard deviation)
+    portfolio_risk = np.sqrt(np.dot(optimal_weights.T, np.dot(cov_matrix, optimal_weights)))
+
+    # Return the optimal weights, return, and risk
+    
+    return equity_data_list,portfolio_return,portfolio_risk
